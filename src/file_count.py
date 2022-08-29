64d47201c45509e8576b96dab103fb38779eee54
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:51:27 -0400

    Update file_count.py

--- src/file_count.py
+++ src/file_count.py
@@ -3,25 +3,10 @@
 import sys
 
 def file_count(filename):
-    total_lines = 0
-    total_words = 0
-    total_chars = 0
-    
-    with open(filename, "r") as f:
-        for line in f:
-            total_lines += 1
-            total_chars += len(line)
-            if line != "\n":
-                total_words += len(line.split(" "))
-    
-    return (total_lines, total_words, total_chars)
+    pass
 
 def main():
-    
-    for file in sys.argv[1:]:
-        data = file_count(file)
-        print(f"{data[0]}\t{data[1]}\t{data[2]}\t{file}")
-
+    pass
 
 if __name__ == "__main__":
     main()
